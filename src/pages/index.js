275307bb8c1b00f3342d4cf924e23a26e948c441
paulrobertpine <paulrobertpine@gmail.com>
2022-07-25 10:13:54 -0400

    search widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,67 @@
 import React, { useState } from "react"
 import { graphql, Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { MdClear } from "react-icons/md"
 import Layout from "../components/layout"
 const title = "Food Buddies"
 
 export default function Home({ data }) {
   const { allMarkdownRemark } = data
   const posts = allMarkdownRemark.nodes
-
   const [search, setSearch] = useState("")
-  const [shuffled, setShuffled] = useState(1)
+
+  let filteredPosts = posts.filter((post) => {
+    return (
+      post.frontmatter.title.toLowerCase().indexOf(search.toLowerCase()) !== -1
+    )
+  })
+
+  function showPosts(posts) {
+    if (posts.length > 0) {
+      return (
+        <ol className="posts-grid">
+          {posts.map((post) => {
+            const postImage = getImage(post.frontmatter.cover)
+            return (
+              <li key={post.fields.slug} className="posts-grid-item">
+                <Link to={post.fields.slug}>
+                  <GatsbyImage
+                    image={postImage}
+                    alt={post.frontmatter.title + " cover image"}
+                  />
+                  <section className="chunk">
+                    <h2>{post.frontmatter.title}</h2>
+                  </section>
+                </Link>
+              </li>
+            )
+          })}
+        </ol>
+      )
+    } else {
+      return <p>No recipes found</p>
+    }
+  }
 
   return (
     <Layout title={title}>
       <article id="home">
         <header className="page-header">
-          <section className="container">search widget, etc</section>
+          <section className="container">
+            <span id="filter-search" className="chunk">
+              <input
+                type="text"
+                placeholder="🔍 Search recipes"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button onClick={() => setSearch("")}>
+                {search && <MdClear />}
+              </button>
+            </span>
+          </section>
         </header>
-        <section className="container">
-          <ol className="posts-grid">
-            {posts.map((post) => {
-              const postImage = getImage(post.frontmatter.cover)
-              return (
-                <li key={post.fields.slug} className="posts-grid-item">
-                  <Link to={post.fields.slug}>
-                    <GatsbyImage
-                      image={postImage}
-                      alt={post.frontmatter.title + " cover image"}
-                    />
-                    <section className="chunk">
-                      <h2>{post.frontmatter.title}</h2>
-                    </section>
-                  </Link>
-                </li>
-              )
-            })}
-          </ol>
-        </section>
+        <section className="container">{showPosts(filteredPosts)}</section>
       </article>
     </Layout>
   )
@@ -61,18 +86,3 @@ export const query = graphql`
     }
   }
 `
-
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex--
-    ;[array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ]
-  }
-
-  return array
-}
