ec9a1a3a31c948b7149eb8d09eeaa7f1cc07664b
paulrobertpine <paulrobertpine@gmail.com>
2022-07-26 16:29:20 -0400

    add 404

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+const title = "404"
+
+export default function fourOFour() {
+  return (
+    <Layout title={title}>
+      <article id="home">
+        <section className="container"><h1>404</h1></section>
+      </article>
+    </Layout>
+  )
+}
